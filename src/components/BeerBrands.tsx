cb5230c85874a1b28e25432ebcb943f2cd27e7ee
Shashank Neelagiri <sneelagiri@outlook.com>
2020-04-09 13:19:25 +0200

    created a class component template to display beer brands

---
+++ src/components/BeerBrands.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import * as superagent from "superagent";
+interface Props {}
+interface State {}
+
+export default class BeerBrands extends Component<Props, State> {
+  state = {};
+  componentDidMount() {}
+  render() {
+    return <div></div>;
+  }
+}
